cb44ecf894960e5700265409cd60f0b015ca532b
Masudha Meher <55777560+samflab@users.noreply.github.com>
2022-02-24 12:08:15 +0530

    Update 3.js

--- week 4 - FP/practice ques by Akanksha/3.js
+++ week 4 - FP/practice ques by Akanksha/3.js
@@ -1,1 +1,5 @@
 //1. Given an array. Write a function that takes in the given array and prints only the numbers which are less than 8 and also an even number.
+const arr = [1, 2, 3, 58, 5, 6, 24, 8, 15, 4];
+
+console.log(arr.filter(ele => ele % 2 === 0 && ele <= 8));
+//[2,6,8,4]

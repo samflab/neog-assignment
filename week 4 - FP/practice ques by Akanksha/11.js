c586dbeb75c22bc7d42aeef7509ce9e6248e1e4f
Masudha Meher <55777560+samflab@users.noreply.github.com>
2022-02-25 13:21:11 +0530

    Create 11.js

---
+++ week 4 - FP/practice ques by Akanksha/11.js
@@ -0,0 +1,7 @@
+//Given an array. Write a function to join all elements of the array with a hyphen in between them.
+const arr = ["Violet", "Indigo", "Blue", "Green", "Yellow", "Orange", "Red"]
+
+const hyphenate = (arr) => arr.join("-")
+
+console.log(hyphenate(arr))
+//Violet-Indigo-Blue-Green-Yellow-Orange-Red
